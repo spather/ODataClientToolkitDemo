39f97e447c21f97b23b859cde38c6800bb332a5a
Shyamalan Pather <shyam.pather@gmail.com>
2011-07-22 14:16:52 -0700

    Initial Commit

---
+++ ODataClientToolkitDemo/ODataClientToolkitDemoAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  ODataClientToolkitDemoAppDelegate.h
+//  ODataClientToolkitDemo
+//
+//  Created by Shyamalan Pather on 7/22/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ODataClientToolkitDemoAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
